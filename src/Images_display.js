b83e64f5ac9c0ace64755b77cc765a22cb6268de
Adi <akumatha@gmail.com>
2022-10-07 17:12:36 +0530

    first props com

---
+++ src/Images_display.js
@@ -0,0 +1,139 @@
+import React from 'react'
+
+const Images_display = () => {
+ const book_author = "Robin";
+ return (
+  <article className='book'>
+   
+   <img src="https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg" alt="amazon" />
+   <h3 style={{margin:"0px 50px"}}>{book_author}</h3>
+   {/* <h3 style={{margin:"0px 50px"}}>{book_author.toUpperCase()}</h3> */}
+
+   <p style={{margin:"0px 50px"}}>Robin Wieruch is a German software and web engineer who is dedicated to learn and teach programming in JavaScript. After obtaining his master's degree in computer science, he continued learning every day on his own. He gained experience from the startup world where he used JavaScript excessively during his professional time and spare time. It gave him the opportunity to teach others about these topics.
+
+For a few years, Robin worked closely with a great team of engineers at a company called Small Improvements developing large scale applications. The company offers a SaaS product that enables the customers to give feedback to their companies. This application is developed using JavaScript in the frontend and Java in the backend. In the frontend, the first iteration was written in Java with the Wicket Framework and jQuery. When the first generation of SPAs became popular, the company migrated to Angular 1.x for the frontend application. After using Angular for more than two years, it became clear that Angular wasn't the best solution to work with state intense applications back in the days. That's why the company made the final jump to React and Redux that has enabled it to operate on a large scale successfully.
+
+
+
+</p>
+   <button style={{color:"red",fontSize:"20px",margin:"0px 50px"}}>499/-</button>
+   <button style={{color:"red",fontSize:"20px"}}>Cancel</button>
+
+
+
+     
+   <img src="https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg" alt="amazon" />
+   <h3 style={{margin:"0px 50px"}}>{book_author}</h3>
+   {/* <h3 style={{margin:"0px 50px"}}>{book_author.toUpperCase()}</h3> */}
+
+   <p style={{margin:"0px 50px"}}>Robin Wieruch is a German software and web engineer who is dedicated to learn and teach programming in JavaScript. After obtaining his master's degree in computer science, he continued learning every day on his own. He gained experience from the startup world where he used JavaScript excessively during his professional time and spare time. It gave him the opportunity to teach others about these topics.
+
+For a few years, Robin worked closely with a great team of engineers at a company called Small Improvements developing large scale applications. The company offers a SaaS product that enables the customers to give feedback to their companies. This application is developed using JavaScript in the frontend and Java in the backend. In the frontend, the first iteration was written in Java with the Wicket Framework and jQuery. When the first generation of SPAs became popular, the company migrated to Angular 1.x for the frontend application. After using Angular for more than two years, it became clear that Angular wasn't the best solution to work with state intense applications back in the days. That's why the company made the final jump to React and Redux that has enabled it to operate on a large scale successfully.
+
+
+
+</p>
+   <button style={{color:"red",fontSize:"20px",margin:"0px 50px"}}>499/-</button>
+   <button style={{color:"red",fontSize:"20px"}}>Cancel</button>
+
+
+
+
+
+     
+   <img src="https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg" alt="amazon" />
+   <h3 style={{margin:"0px 50px"}}>{book_author}</h3>
+   {/* <h3 style={{margin:"0px 50px"}}>{book_author.toUpperCase()}</h3> */}
+
+   <p style={{margin:"0px 50px"}}>Robin Wieruch is a German software and web engineer who is dedicated to learn and teach programming in JavaScript. After obtaining his master's degree in computer science, he continued learning every day on his own. He gained experience from the startup world where he used JavaScript excessively during his professional time and spare time. It gave him the opportunity to teach others about these topics.
+
+For a few years, Robin worked closely with a great team of engineers at a company called Small Improvements developing large scale applications. The company offers a SaaS product that enables the customers to give feedback to their companies. This application is developed using JavaScript in the frontend and Java in the backend. In the frontend, the first iteration was written in Java with the Wicket Framework and jQuery. When the first generation of SPAs became popular, the company migrated to Angular 1.x for the frontend application. After using Angular for more than two years, it became clear that Angular wasn't the best solution to work with state intense applications back in the days. That's why the company made the final jump to React and Redux that has enabled it to operate on a large scale successfully.
+
+
+
+</p>
+   <button style={{color:"red",fontSize:"20px",margin:"0px 50px"}}>499/-</button>
+   <button style={{color:"red",fontSize:"20px"}}>Cancel</button>
+
+
+
+
+
+     
+   <img src="https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg" alt="amazon" />
+   <h3 style={{margin:"0px 50px"}}>{book_author}</h3>
+   {/* <h3 style={{margin:"0px 50px"}}>{book_author.toUpperCase()}</h3> */}
+
+   <p style={{margin:"0px 50px"}}>Robin Wieruch is a German software and web engineer who is dedicated to learn and teach programming in JavaScript. After obtaining his master's degree in computer science, he continued learning every day on his own. He gained experience from the startup world where he used JavaScript excessively during his professional time and spare time. It gave him the opportunity to teach others about these topics.
+
+For a few years, Robin worked closely with a great team of engineers at a company called Small Improvements developing large scale applications. The company offers a SaaS product that enables the customers to give feedback to their companies. This application is developed using JavaScript in the frontend and Java in the backend. In the frontend, the first iteration was written in Java with the Wicket Framework and jQuery. When the first generation of SPAs became popular, the company migrated to Angular 1.x for the frontend application. After using Angular for more than two years, it became clear that Angular wasn't the best solution to work with state intense applications back in the days. That's why the company made the final jump to React and Redux that has enabled it to operate on a large scale successfully.
+
+
+
+</p>
+   <button style={{color:"red",fontSize:"20px",margin:"0px 50px"}}>499/-</button>
+   <button style={{color:"red",fontSize:"20px"}}>Cancel</button>
+
+
+
+
+
+
+     
+   <img src="https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg" alt="amazon" />
+   <h3 style={{margin:"0px 50px"}}>{book_author}</h3>
+   {/* <h3 style={{margin:"0px 50px"}}>{book_author.toUpperCase()}</h3> */}
+
+   <p style={{margin:"0px 50px"}}>Robin Wieruch is a German software and web engineer who is dedicated to learn and teach programming in JavaScript. After obtaining his master's degree in computer science, he continued learning every day on his own. He gained experience from the startup world where he used JavaScript excessively during his professional time and spare time. It gave him the opportunity to teach others about these topics.
+
+For a few years, Robin worked closely with a great team of engineers at a company called Small Improvements developing large scale applications. The company offers a SaaS product that enables the customers to give feedback to their companies. This application is developed using JavaScript in the frontend and Java in the backend. In the frontend, the first iteration was written in Java with the Wicket Framework and jQuery. When the first generation of SPAs became popular, the company migrated to Angular 1.x for the frontend application. After using Angular for more than two years, it became clear that Angular wasn't the best solution to work with state intense applications back in the days. That's why the company made the final jump to React and Redux that has enabled it to operate on a large scale successfully.
+
+
+
+</p>
+   <button style={{color:"red",fontSize:"20px",margin:"0px 50px"}}>499/-</button>
+   <button style={{color:"red",fontSize:"20px"}}>Cancel</button>
+
+
+
+
+
+
+     
+   <img src="https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg" alt="amazon" />
+   <h3 style={{margin:"0px 50px"}}>{book_author}</h3>
+   {/* <h3 style={{margin:"0px 50px"}}>{book_author.toUpperCase()}</h3> */}
+
+   <p style={{margin:"0px 50px"}}>Robin Wieruch is a German software and web engineer who is dedicated to learn and teach programming in JavaScript. After obtaining his master's degree in computer science, he continued learning every day on his own. He gained experience from the startup world where he used JavaScript excessively during his professional time and spare time. It gave him the opportunity to teach others about these topics.
+
+For a few years, Robin worked closely with a great team of engineers at a company called Small Improvements developing large scale applications. The company offers a SaaS product that enables the customers to give feedback to their companies. This application is developed using JavaScript in the frontend and Java in the backend. In the frontend, the first iteration was written in Java with the Wicket Framework and jQuery. When the first generation of SPAs became popular, the company migrated to Angular 1.x for the frontend application. After using Angular for more than two years, it became clear that Angular wasn't the best solution to work with state intense applications back in the days. That's why the company made the final jump to React and Redux that has enabled it to operate on a large scale successfully.
+
+
+
+</p>
+   <button style={{color:"red",fontSize:"20px",margin:"0px 50px"}}>499/-</button>
+   <button style={{color:"red",fontSize:"20px"}}>Cancel</button>
+
+
+
+
+
+     
+   <img src="https://m.media-amazon.com/images/I/41m+5+-JRNL.jpg" alt="amazon" />
+   <h3 style={{margin:"0px 50px"}}>{book_author}</h3>
+   {/* <h3 style={{margin:"0px 50px"}}>{book_author.toUpperCase()}</h3> */}
+
+   <p style={{margin:"0px 50px"}}>Robin Wieruch is a German software and web engineer who is dedicated to learn and teach programming in JavaScript. After obtaining his master's degree in computer science, he continued learning every day on his own. He gained experience from the startup world where he used JavaScript excessively during his professional time and spare time. It gave him the opportunity to teach others about these topics.
+
+For a few years, Robin worked closely with a great team of engineers at a company called Small Improvements developing large scale applications. The company offers a SaaS product that enables the customers to give feedback to their companies. This application is developed using JavaScript in the frontend and Java in the backend. In the frontend, the first iteration was written in Java with the Wicket Framework and jQuery. When the first generation of SPAs became popular, the company migrated to Angular 1.x for the frontend application. After using Angular for more than two years, it became clear that Angular wasn't the best solution to work with state intense applications back in the days. That's why the company made the final jump to React and Redux that has enabled it to operate on a large scale successfully.
+
+
+
+</p>
+   <button style={{color:"red",fontSize:"20px",margin:"0px 50px"}}>499/-</button>
+   <button style={{color:"red",fontSize:"20px"}}>Cancel</button>
+  </article>
+ )
+}
+
+export default Images_display
---
+++ src/twocomponentsname.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const twocomponentsname = () => {
+  return (
+    <div className="second_data">
+<h2>React js </h2>
+<h3 className='third'>Java Script Learn.........</h3>
+
+
+
+
+      
+    </div>
+  );
+}
+
+export default twocomponentsname;
